d9aa1eca2a3b3e8327ada159a12cca437fbca5f4
Mikko Hellsing <mikko@aino.se>
2011-02-05 01:15:25 +0100

    fixing setup

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
-from os.path import abspath, dirname, join as pjoin
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-
-fn = abspath(pjoin(dirname(__file__), 'README.rst'))
-fp = open(fn, 'r')
-long_description = fp.read()
-fp.close()
 
 setup(
     name='django-rte',
-    version='0.1',
+    version='0.2',
     url='https://github.com/aino/django-rte',
     license='BSD',
     author='Mikko Hellsing',
     author_email='mikko@aino.se',
     description='Rich text editor for Django',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -29,11 +20,8 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Framework :: Django',
     ],
-    packages=[
-        'rte',
-    ],
+    packages=find_packages(),
     platforms='any',
-    # we don't want eggs
     zip_safe=False,
 )
 
