b0e173bd7e4ed197b9a6aa1fb6e7f6d91f156be2
Mikko Hellsing <mikko@aino.se>
2011-02-05 01:33:00 +0100

    fail

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from setuptools import setup, find_packages
 setup(
     name='django-rte',
     version='0.2',
-    url='https://github.com/aino/django-rte',
-    license='BSD',
-    author='Mikko Hellsing',
-    author_email='mikko@aino.se',
     description='Rich text editor for Django',
     long_description=open('README.rst').read(),
+    author='Mikko Hellsing',
+    author_email='mikko@aino.se',
+    license='BSD',
+    url='https://github.com/aino/django-rte',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -20,8 +23,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Framework :: Django',
     ],
-    packages=find_packages(),
-    platforms='any',
-    zip_safe=False,
 )
 
