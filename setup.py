aeb361dabe08a16869967ace3e01be630fe8abad
Mikko Hellsing <mikko@aino.se>
2011-06-14 16:32:56 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rte',
-    version='0.2',
+    version='0.3',
     description='Rich text editor for Django',
     long_description=open('README.rst').read(),
     author='Mikko Hellsing',
